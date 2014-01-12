217946712d8ab719d227636c66ddadb97e553ed8
David Gauld <dcgauld@gmail.com>
2014-01-12 23:17:17 +0000

    Updated distribution files

--- dist/colorama-1.0.0.js
+++ dist/colorama-1.0.0.js
@@ -25,23 +25,31 @@ Colorama = module.exports = require("./lib/colorama");
 },{"./lib/colorama":2}],2:[function(require,module,exports){
 'use strict';
 
+var conversions = require('./conversions');
+
 module.exports = function(color) {
   return new Colorama(color);
 };
 
+/*
+ * The constructor for a Colorama color. Tries to find out what you passed it
+ * and attempts to convert it to rgb format (storing it in this.base).
+ */
 function Colorama(color) {
-  this.rgb = {
-    r: 0,
-    g: 0,
-    b: 0
-  };
+  this.base = { r: 0, g: 0, b: 0 };
   this.formats = ['hex', 'rgb', 'hsl', 'hsv'];
   switch (typeof color) {
   case 'string':
     if (this._parseHexString(color)) {
       this._set('hex', this._parseHexString(color));
+    } else if (this._parseRgbString(color)) {
+      this._set('rgb', this._parseRgbString(color));
+    } else if (this._parseHslString(color)) {
+      this._set('hsl', this._parseHslString(color));
+    } else if (this._parseHsvString(color)) {
+      this._set('hsv', this._parseHsvString(color));
     } else {
-      return new Error('No valid color was provided.');
+      return false;
     }
     return this;
   case 'object':
@@ -52,44 +60,142 @@ function Colorama(color) {
     } else if (color.v || color.value) {
       this._set('hsv', color);
     } else {
-      return new Error('No valid color was provided.');
+      return false;
     }
     return this;
   }
 }
 
 Colorama.prototype = {
-  _get: function(key) {
-    if (this.formats.indexOf(key) !== -1) {
-      if (typeof conversions[key + 'ToRgb'] === 'function') {
-        return conversions['rgbTo' + key]();
-      }
-    }
-    throw new Error('Invalid color format specified.');
+  /*
+   * Clones a Colorama object, useful for when a user wants to manipulate a
+   * color but keep the original. Modified objects are edited by reference e.g.
+   *
+   *    var c = Colorama('#ff0000');
+   *    var c2 = c;
+   *    c2.darken();
+   *
+   * Both c and c2 will be updated as the variables are pointing to the same
+   * object. The following example shows the correct method.
+   *
+   *    var c = Colorama('#ff0000');
+   *    var c2 = c.clone();
+   *    c2.darken();
+   *
+   * Boom!
+   */
+  clone: function() {
+    return new Colorama(this.hex());
   },
 
-  // Accepts either string (hex) or object
+  /*
+   * Methods for getting or setting all values within a particular color type,
+   * e.g. rgb([20, 30, 40]).
+   */
+  hex: function(color) {
+    return this._set('hex', color);
+  },
+  rgb: function(color) {
+    return this._set('rgb', color);
+  },
+  hsl: function(color) {
+    return this._set('hsl', color);
+  },
+  hsv: function(color) {
+    return this._set('hsv', color);
+  },
+  css: function() {
+    return this.string('hex');
+  },
+
+  /*
+   * Methods for getting a string version of a particular color types, e.g.
+   * 'rgb(20, 30, 40)'. Useful for CSS and debugging.
+   */
+  string: function(format) {
+    if (format === undefined) {
+      format = 'rgb';
+    }
+    var color = this._get(format);
+    switch (format) {
+    case 'hex':
+      return '#' + color;
+    case 'rgb':
+      return 'rgb(' + color.r + ', ' + color.g + ', ' + color.b + ')';
+    case 'hsl':
+      return 'hsl(' + color.h + ', ' + color.s + '%, ' + color.l + '%)';
+    case 'hsv':
+      return 'hsv(' + color.h + ', ' + color.s + '%, ' + color.v + '%)';
+    }
+    return null;
+  },
+
+  /*
+   * Retrieve the color's base color in a particular format. Converts this.base
+   * into the format specified.
+   */
+  _get: function(key) {
+    if (this.formats.indexOf(key) !== -1) {
+      if (key === 'rgb') {
+        return this.base;
+      } else if (typeof conversions['rgbTo' + key.charAt(0).toUpperCase() + key.slice(1)] === 'function') {
+        return conversions['rgbTo' + key.charAt(0).toUpperCase() + key.slice(1)](this.base);
+      }
+    }
+    return false;
+  },
+
+  /*
+   * Sets this.base to a particular color, after converting it to a specified
+   * format. Particularly useful for editing existing colors.
+   */
   _set: function(key, value) {
     if (this.formats.indexOf(key) !== -1) {
       if (value === undefined) {
         return this._get(key);
       }
-      if (typeof conversions[key + 'ToRgb'] === 'function') {
-        this.rgb = conversions[key + 'ToRgb'](value);
+      if (value instanceof Array) {
+        switch (key) {
+        case 'hex':
+          this.base = conversions.hexToRgb(value);
+          break;
+        case 'rgb':
+          this.base = { r: value[0], g: value[1], b: value[2] };
+          break;
+        case 'hsl':
+          this.base = conversions.hslToRgb({ h: value[0], s: value[1], l: value[2] });
+          break;
+        case 'hsv':
+          this.base = conversions.hsvToRgb({ h: value[0], s: value[1], v: value[2] });
+          break;
+        }
+        return this.base;
+      } else {
+        if (key === 'rgb') {
+          this.base = value;
+          return this.base;
+        } else {
+          if (typeof conversions[key + 'ToRgb'] === 'function') {
+            this.base = conversions[key + 'ToRgb'](value);
+            return this.base;
+          }
+        }
       }
-      return true;
+      return false;
     }
-    throw new Error('Invalid color format specified.');
+    return false;
   },
 
+  /*
+   * Methods used for parsing colors from string form. E.g. 'rgb(20, 30, 40)'
+   * and '#ff00ff'.
+   */
   _parseHexString: function(color) {
     if (!color) {
       return;
     }
-
     var match = color.match(/^#?(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/);
     if (match) {
-      console.log('hmm');
       return match[1];
     }
     return false;
@@ -98,78 +204,142 @@ Colorama.prototype = {
     if (!color) {
       return;
     }
-    var rgb = {
-      r: 0,
-      g: 0,
-      b: 0
-    };
-    var match = color.match(/^rgba?\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(?:,\s*([\d\.]+)\s*)?\)$/),
-        i;
+    var rgb = {},
+        match = color.match(/^rgba?\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(?:,\s*([\d\.]+)\s*)?\)$/);
     if (match) {
-      for (i = 0; i < rgb.length; i++) {
-        rgb[i] = parseInt(match[i + 1], 10);
-      }
-    } else {
-      rgb = null;
+      rgb.r = this._scale(parseInt(match[1], 10), 0, 255);
+      rgb.g = this._scale(parseInt(match[2], 10), 0, 255);
+      rgb.b = this._scale(parseInt(match[3], 10), 0, 255);
+      return rgb;
     }
-    if (rgb) {
-      for (i = 0; i < rgb.length; i++) {
-        rgb[i] = this._scale(rgb[i], 0, 255);
-      }
-    }
-    return rgb;
+    return false;
   },
   _parseHslString: function(color) {
     if (!color) {
       return;
     }
-    var hslMatch = color.match(/^hsla?\(\s*(\d+)\s*,\s*([\d\.]+)%\s*,\s*([\d\.]+)%\s*(?:,\s*([\d\.]+)\s*)?\)/);
-    if (hslMatch) {
-      var h = this._scale(parseInt(hslMatch[1], 10), 0, 360),
-          s = this._scale(parseFloat(hslMatch[2]), 0, 100),
-          l = this._scale(parseFloat(hslMatch[3]), 0, 100);
-      return { h: h, s: s, l: l };
+    var hsl = {},
+        match = color.match(/^hsla?\(\s*(\d+)\s*,\s*([\d\.]+)%\s*,\s*([\d\.]+)%\s*(?:,\s*([\d\.]+)\s*)?\)/);
+    if (match) {
+      hsl.h = this._scale(parseInt(match[1], 10), 0, 360),
+      hsl.s = this._scale(parseFloat(match[2]), 0, 100),
+      hsl.l = this._scale(parseFloat(match[3]), 0, 100);
+      return hsl;
     }
-    return null;
-  },
-  _parseHsvString: function(color) {
-    return color;
+    return false;
   },
 
+  /*
+   * Utility methods. Always seem to be left until last, but pretty important.
+   */
   _scale: function(num, min, max) {
     return Math.min(Math.max(min, num), max);
-  },
-
-  string: function() {
-    return this.rgb;
   }
 };
+},{"./conversions":3}],3:[function(require,module,exports){
+'use strict';
 
-var conversions = {
+module.exports = {
+  rgbToHex: function(color) {
+    var hex = '';
+    var pad = function(n) {
+      while (n.length < 2) {
+        n = '0' + n;
+      }
+      return n;
+    };
+    hex += pad(color.r.toString(16));
+    hex += pad(color.g.toString(16));
+    hex += pad(color.b.toString(16));
+    return hex;
+  },
+  rgbToHsl: function(color) {
+    var r = color.r / 255,
+        g = color.g / 255,
+        b = color.b / 255,
+        min = Math.min(r, g, b),
+        max = Math.max(r, g, b),
+        delta = max - min,
+        h, s, l;
+    if (max === min) {
+      h = 0;
+    } else if (r === max) {
+      h = (g - b) / delta; 
+    } else if (g === max) {
+      h = 2 + (b - r) / delta; 
+    } else if (b === max) {
+      h = 4 + (r - g) / delta;
+    }
+    h = Math.min(h * 60, 360);
+    if (h < 0) {
+      h += 360;
+    }
+    l = (min + max) / 2;
+    if (max === min) {
+      s = 0;
+    } else if (l <= 0.5) {
+      s = delta / (max + min);
+    } else {
+      s = delta / (2 - max - min);
+    }
+    return { h: h, s: s * 100, l: l * 100 };
+  },
+  rgbToHsv: function(color) {
+    var r = color.r,
+        g = color.g,
+        b = color.b,
+        min = Math.min(r, g, b),
+        max = Math.max(r, g, b),
+        delta = max - min,
+        h, s, v;
+
+    if (max === 0) {
+      s = 0;
+    } else {
+      s = (delta/max * 1000)/10;
+    }
+    if (max === min) {
+      h = 0;
+    } else if (r === max) {
+      h = (g - b) / delta; 
+    } else if (g === max) {
+      h = 2 + (b - r) / delta; 
+    } else if (b === max) {
+      h = 4 + (r - g) / delta;
+    }
+    h = Math.min(h * 60, 360);
+    if (h < 0) {
+      h += 360;
+    }
+    v = ((max / 255) * 1000) / 10;
+    return { h: h, s: s, v: v };
+  },
   hexToRgb: function(color) {
     if (color === undefined) {
       return;
     }
-    if (typeof color === 'string') {
-      color = color.replace('#', '');
-      if (color.length < 6) {
-        color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
-      }
-      var r = parseInt(color.substring(0, 2), 16),
-          g = parseInt(color.substring(2, 4), 16),
-          b = parseInt(color.substring(4, 6), 16);
-      return { r: r, g: g, b: b };
+    color = color.replace('#', '');
+    if (color.length < 6) {
+      color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
     }
-    throw new Error('Invalid hex color specified.');
+    var r = parseInt(color.substring(0, 2), 16),
+        g = parseInt(color.substring(2, 4), 16),
+        b = parseInt(color.substring(4, 6), 16);
+    return { r: r, g: g, b: b };
   },
   hslToRgb: function(color) {
-    var h = color.h / 360,
+    var rgb = {},
+        array = [],
+        h = color.h / 360,
         s = color.s / 100,
         l = color.l / 100,
-        t1, t2, t3, rgb, val, i;
+        t1, t2, t3, val, i;
     if (s === 0) {
       val = l * 255;
-      return [val, val, val];
+      rgb.r = val;
+      rgb.g = val;
+      rgb.b = val;
+      return rgb;
     }
     if (l < 0.5) {
       t2 = l * (1 + s);
@@ -177,7 +347,6 @@ var conversions = {
       t2 = l + s - l * s;
     }
     t1 = 2 * l - t2;
-    rgb = [0, 0, 0];
     for (i = 0; i < 3; i++) {
       t3 = h + 1 / 3 * - (i - 1);
       t3 < 0 && t3++;
@@ -191,8 +360,11 @@ var conversions = {
       } else {
         val = t1;
       }
-      rgb[i] = val * 255;
+      array[i] = val * 255;
     }
+    rgb.r = array[0];
+    rgb.g = array[1];
+    rgb.b = array[2];
     return rgb;
   },
   hsvToRgb: function(color) {
